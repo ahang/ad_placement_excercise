118549fa09fd26360e8620c0a72479e6335f0df2
Andy Hang <hang.andy.t@gmail.com>
2018-06-19 14:00:22 -0500

    added fast-csv to parse the csv file. currently reading csv for now.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const fs = require('fs');
+const csv = require('fast-csv');
 const placement = 'placement.csv';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
+  let placementData = [];
+  fs.createReadStream(placement)
+  .pipe(csv())
+  .on('data', (data) => {
+    placementData = [...placementData, data]
+  })
+  .on('end', (data) => {
+    console.log(placementData);
+  })
   res.send({ 'Hello': 'World' });
 });
 
