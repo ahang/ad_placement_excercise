32f958526beb11832129b9645ce73962664b5156
Andy Hang <hang.andy.t@gmail.com>
2018-06-21 20:57:23 -0500

    created an obj to handle and each unique impression

--- index.js
+++ index.js
@@ -48,13 +48,45 @@ app.get("/getCustom/:start_date/:end_date", (req, res) => {
   const sDate = Date.parse(moment(req.params.start_date).format("MM/D/YYYY"));
   const eDate = Date.parse(moment(req.params.end_date).format("MM/D/YYYY"));
   let dataRange = [];
+  const placementImpressions = {
+    placement1: 0,
+    placement2: 0,
+    placement3: 0,
+    placement4: 0
+  };
   dataRange = deliveryObj.filter(placement => {
     const pDate = Date.parse(placement.date);
     if (pDate >= sDate && pDate <= eDate) {
       return placement;
     }
   });
-  const 
+
+  dataRange.forEach(data => {
+    switch (data.placement_id) {
+      case "1":
+        placementImpressions["placement1"] =
+          placementImpressions["placement1"] + parseInt(data.impressions);
+        break;
+      case "2":
+        placementImpressions["placement2"] =
+          placementImpressions["placement2"] + parseInt(data.impressions);
+        break;
+      case "3":
+        placementImpressions["placement3"] =
+          placementImpressions["placement3"] + parseInt(data.impressions);
+        break;
+      case "4":
+        placementImpressions["placement4"] =
+          placementImpressions["placement4"] + parseInt(data.impressions);
+        break;
+    }
+  });
+  console.log(dataRange);
+  console.log(placementImpressions);
+  // dataRange.map(data => {
+
+  // });
+  // console.log(placementImpressions);
 });
 
 app.listen(PORT, () => {
