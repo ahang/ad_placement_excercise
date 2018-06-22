888933c4ba95dc22c485a16298188b7c821b1feb
Andy Hang <hang.andy.t@gmail.com>
2018-06-21 20:18:26 -0500

    created a function to initialize data in memory

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
-const express = require('express');
-const moment = require('moment');
-const csv = require('csvtojson');
-const placement = 'placement.csv';
-const delivery = 'delivery.csv';
+const express = require("express");
+const moment = require("moment");
+const csv = require("csvtojson");
+const placement = "placement.csv";
+const delivery = "delivery.csv";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/getPlacementData', (req, res) => {
+let placementObj;
+let deliveryObj;
+
+const initializeData = () => {
   csv()
-  .fromFile(placement)
-  .then((placementObj) => {
-    res.json(placementObj)
-  })
+    .fromFile(placement)
+    .then(pObj => {
+      placementObj = pObj;
+    })
+    .then(() => {
+      csv()
+        .fromFile(delivery)
+        .then(dObj => {
+          deliveryObj = dObj;
+        });
+    });
+};
+
+initializeData();
+
+app.get("/getPlacementData", (req, res) => {
+  res.json(placementObj);
 });
 
-app.get('/getTotalImpressions/:id/:cpm', (req, res) => {
+app.get("/getTotalImpressions/:id/:cpm", (req, res) => {
   const id = req.params.id;
   const cpm = req.params.cpm;
-  csv()
-  .fromFile(delivery)
-  .then((deliveryObj) => {
-    let totalImpressions = 0;
-    deliveryObj.map(delivery => {
-      if(delivery.placement_id === id) {
-        totalImpressions = totalImpressions + parseInt(delivery.impressions);
-      }
-    })
-    const totalCPM = totalImpressions * cpm;
-    res.json({ totalImpressions, totalCPM });
-  })
-})
+  let totalImpressions = 0;
+  deliveryObj.map(delivery => {
+    if (delivery.placement_id === id) {
+      totalImpressions = totalImpressions + parseInt(delivery.impressions);
+    }
+  });
+  const totalCPM = totalImpressions * cpm;
+  res.json({ totalImpressions, totalCPM });
+});
 
-app.get('/getCustom/:start_date/:end_date', (req, res) => {
-  const sDate = Date.parse(moment(req.params.start_date).format('MM/D/YYYY'));
-  const eDate = Date.parse(moment(req.params.end_date).format('MM/D/YYYY'));
-  csv()
-  .fromFile(delivery)
-  .then(deliveryObj => {
-    const dataRange = deliveryObj.filter(placement => {
-      const pDate = Date.parse(placement.date);
-      if(pDate >= sDate && pDate <= eDate) {
-        return placement;
-      }
-    })
-    console.log(dataRange);
-  })
-})
+app.get("/getCustom/:start_date/:end_date", (req, res) => {
+  const sDate = Date.parse(moment(req.params.start_date).format("MM/D/YYYY"));
+  const eDate = Date.parse(moment(req.params.end_date).format("MM/D/YYYY"));
+  let dataRange = [];
+  dataRange = deliveryObj.filter(placement => {
+    const pDate = Date.parse(placement.date);
+    if (pDate >= sDate && pDate <= eDate) {
+      return placement;
+    }
+  });
+  const 
+});
 
 app.listen(PORT, () => {
   console.log(`Server Running`);
