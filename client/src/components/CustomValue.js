8ef1e1f8d18e1a07ce97bdf097ed47b21f9f88c4
Andy Hang <hang.andy.t@gmail.com>
2018-06-22 08:53:18 -0500

    adjusted formatting

--- client/src/components/CustomValue.js
+++ client/src/components/CustomValue.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import moment from 'moment';
-import NumberFormat from 'react-number-format';
+import React from "react";
+import moment from "moment";
+import NumberFormat from "react-number-format";
 
 const CustomValue = ({ info }) => {
-  if(info.showData) {
+  if (info.showData) {
     const { startDate, endDate } = info;
     const { total_impressions, total_cpm } = info.customData;
-    return <div>
+    return (
+      <div>
         <p>
-          Total ({moment(startDate).format("MM/D/YYYY")} - {moment(endDate).format("MM/D/YYYY")}):
-          <NumberFormat value={total_impressions} displayType={"text"} thousandSeparator={true} /> impressions,
-          <NumberFormat value={total_cpm} displayType={"text"} prefix={'$'}
-          thousandSeparator={true} />
+          Total ({moment(startDate).format("MM/D/YYYY")} -
+          {moment(endDate).format("MM/D/YYYY")})
+          <span>: </span>
+        <NumberFormat
+          value={total_impressions}
+          displayType={"text"}
+          thousandSeparator={true}
+        /> impressions,
+        <span> </span>
+        <NumberFormat
+          value={total_cpm}
+          displayType={"text"}
+          prefix={"$"}
+          thousandSeparator={true}
+        />
         </p>
-      </div>;
+      </div>
+    );
   }
   return (
     <div>
       <p>No Custom Dates Selected</p>
     </div>
-  )
-}
+  );
+};
 
 export default CustomValue;
